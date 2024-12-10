37a572150228b58d6a85cfe6892c57625dfe5328
Chris Mann <chris@lesgtrandsvoisins.com>
2024-12-10 10:15:30 +0100

    Working on Vikunja issue

--- configuration.nix
+++ configuration.nix
@@ -67,10 +67,10 @@ in
     (pkgs.callPackage ./etc/sftpgo/sftpgo-plugin-auth/sftpgoPluginAuth.nix { }  )
   ];
   age.secrets = {
-    "keylesgrandsvoisins.vikunja" = { file = ./secrets/keylesgrandsvoisins.vikunja.age;};
+    "keylesgrandsvoisins.vikunja" = { file = ./secrets/keylesgrandsvoisins.vikunja.age; owner = "vikunja";};
     "key.sftpgo" = { file = ./secrets/key.sftpgo.age; owner = "sftpgo";};
     "keycloak.vikunja" = { file = ./secrets/keycloak.vikunja.age;};
-    "email.list" = { file = ./secrets/email.list.age;};
+    "email.list" = { file = ./secrets/email.list.age; group="wwwrun"; mode="770";};
     # "bind.slappasswd" = { file = ./secrets/bind.slappasswd.age;};
     "vikunja.env" = { 
       file = ./secrets/vikunja.env.age; 
