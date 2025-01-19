f9b16bd13f6ba29fd6c6bc51e0abd72a1cc8c86a
Chris Mann <chris@mann.fr>
2025-01-19 12:26:55 +0100

    From T330Roses

--- configuration.nix
+++ configuration.nix
@@ -94,7 +94,8 @@ in
   networking.firewall.allowedTCPPorts = [ 22 80 443 636 ];
   networking.networkmanager = {
     enable = true;
-  }
+    dhcp = "dhcpcd";
+  };
   networking.hostName = "t330roses"; # Define your hostname.
   networking.enableIPv6 = true;
   # networking.firewall.package
