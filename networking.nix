a7778b9d3a4abbfebdc114ff4bfe7f9659d14dd7
Chris Mann <chris@lesgtrandsvoisins.com>
2025-01-22 09:48:41 +0100

    Not activating nfs server

--- networking.nix
+++ networking.nix
@@ -1,10 +1,10 @@
 { config, pkgs, lib, ... }:
 let 
 in {
-  # Networking
-  networking.firewall.allowedTCPPorts = [ 22 80 443 636 53 ];
-  # networking.firewall.allowedUDPPorts = [ 53 67 68 123 ];
-  networking.hostName = "rosest330"; # Define your hostname.
-  networking.enableIPv6 = true;
-  networking.nftables.enable = true; # for now
+  # # Networking
+  # networking.firewall.allowedTCPPorts = [ 22 80 443 636 53 ];
+  # # networking.firewall.allowedUDPPorts = [ 53 67 68 123 ];
+  # networking.hostName = "rosest330"; # Define your hostname.
+  # networking.enableIPv6 = true;
+  # networking.nftables.enable = true; # for now
 }
--- nfs.nix
+++ nfs.nix
@@ -11,18 +11,18 @@ let
   };
 in 
 {
-  services.nfs.server = {
-    enable = true;
-    lockdPort = 4001;
-    mountdPort = 4002;
-    statdPort = 4000;
-    extraNfsdConfig = '''';
-  };
-  fileSystems = {
-    "/export/data1" = {device="/mnt/chrisdatalive";options=["bind"];};  
-  };
-  services.nfs.server.exports = ''
-    /export 192.168.1.0/24(rw,fsid=0,no_subtree_check)
-    /export/data1 192.168.1.0/24(rw,nohide,insecure,no_subtree_check)
-  '';
+  # services.nfs.server = {
+  #   enable = true;
+  #   lockdPort = 4001;
+  #   mountdPort = 4002;
+  #   statdPort = 4000;
+  #   extraNfsdConfig = '''';
+  # };
+  # fileSystems = {
+  #   "/export/data1" = {device="/mnt/chrisdatalive";options=["bind"];};  
+  # };
+  # services.nfs.server.exports = ''
+  #   /export 192.168.1.0/24(rw,fsid=0,no_subtree_check)
+  #   /export/data1 192.168.1.0/24(rw,nohide,insecure,no_subtree_check)
+  # '';
 }
