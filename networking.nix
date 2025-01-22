a8eef18885535b22f473301300e5f5ba3fca95d0
Chris Mann <chris@lesgtrandsvoisins.com>
2025-01-22 09:00:00 +0100

    Firewall permissions for NFS

--- networking.nix
+++ networking.nix
@@ -2,7 +2,13 @@
 let 
 in {
   # Networking
-  networking.firewall.allowedTCPPorts = [ 22 80 443 636 ];
+  networking.firewall.allowedTCPPorts = [ 22 80 443 636 ] + [ 53 ];
+  networking.firewall.allowedUDPPorts = [
+    53
+    67
+    68
+    123
+  ];
   networking.hostName = "rosest330"; # Define your hostname.
   networking.enableIPv6 = true;
   networking.nftables.enable = true;
