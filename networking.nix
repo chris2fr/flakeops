30e344a1ec2b2899b2e289a669bcf4a5b20ee0db
Chris Mann <chris@lesgtrandsvoisins.com>
2025-01-22 16:25:43 +0100

    Not having fun with this firewall setup

--- networking.nix
+++ networking.nix
@@ -1,5 +1,7 @@
 { config, pkgs, lib, ... }:
 let 
+  allowedTCPPorts = [ 22 80 443 636 53 111 2049 ];
+  allowedUDPPorts = [ 53 67 68 123 111 2049 4000 4001 4002 20048 ];
 in {
   networking = {
     firewall = {
@@ -8,8 +10,12 @@ in {
       allowedUDPPorts = [ 53 67 68 123 111 2049 4000 4001 4002 ];
       trustedInterfaces = ["eno2" "enp3s0f0"];
       interfaces."eno2" = {
-        allowedTCPPorts = [ 22 80 443 636 53 111 2049 ];
-        allowedUDPPorts = [ 53 67 68 123 111 2049 4000 4001 4002 20048 ];
+        allowedTCPPorts = allowedTCPPorts;
+        allowedUDPPorts = allowedUDPPorts;
+      };
+      interfaces."enp3s0f0" = {
+        allowedTCPPorts = allowedTCPPorts;
+        allowedUDPPorts = allowedUDPPorts;
       };
     };
     hostName = "rosest330"; 
