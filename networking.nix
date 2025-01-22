43e77a161e90fd1355c39d30020573ad26cd4281
Chris Mann <chris@lesgtrandsvoisins.com>
2025-01-22 10:25:28 +0100

    Adding a basic networking configuration.

--- networking.nix
+++ networking.nix
@@ -1,12 +1,13 @@
 { config, pkgs, lib, ... }:
 let 
 in {
-  networking.firewall.enable = false;
-  # # Networking
-  # networking.firewall.allowedTCPPorts = [ 22 80 443 636 53 ];
-  # # networking.firewall.allowedUDPPorts = [ 53 67 68 123 ];
-  # networking.hostName = "rosest330"; # Define your hostname.
-  # networking.enableIPv6 = true;
-  # networking.nftables.enable = true; # for now
-  
+  networking = {
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 22 80 443 636 53 ];
+      allowedUDPPorts = [ 53 67 68 123 ];
+    };
+    hostName = "rosest330"; 
+    enableIPv6 = true;
+  };
 }
