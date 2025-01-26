0592ebe2bd91c470c8b0b30252ef5e9372e019db
Chris Mann <chris@lesgtrandsvoisins.com>
2025-01-26 12:59:49 +0100

    Cleanup and mlocate

--- configuration.nix
+++ configuration.nix
@@ -52,33 +52,13 @@ in
     "d /srv/rd4 0755 nfsuser users"
   ];
 
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  #services.xserver.xkb = {
-  #  layout = "fr";
-  #  variant = "azerty";
-  #};
-
+environment.systemPackages = with pkgs; [
+  mlocate
+];
 
  services.openssh = {
     ports = [22 22222];
-#    listenAddresses = [
-#      {
-#        addr = "[::]";
-#        port = 22;
-#      }
-#      {
-#        addr = "[::]";
-#        port = 22222;
-#      }
-#    ];
+
     enable = true;
   };
   services.locate.enable = true;
@@ -100,33 +80,6 @@ in
   # Configure console keymap
   console.keyMap = "fr";
 
-  # Enable CUPS to print documents.
-  #services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  #hardware.pulseaudio.enable = false;
-  #security.rtkit.enable = true;
-#  services.pipewire = {
-#    enable = true;
-#    alsa.enable = true;
-#    alsa.support32Bit = true;
-#    pulse.enable = true;
-#    # If you want to use JACK applications, uncomment this
-#    #jack.enable = true;
-#
-#    # use the example session manager (no others are packaged yet so this is enabled by default,
-#    # no need to redefine it in your config for now)
-#    #media-session.enable = true;
-#  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-
-
-  # Install firefox.
-  # programs.firefox.enable = true;
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
@@ -144,20 +97,6 @@ in
     perccli
     lm_sensors
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
