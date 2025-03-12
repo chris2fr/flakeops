6f9e122ef7039e1f94f9db9d4059c8f62651b07b
Chris Mann <chris@lesgtrandsvoisins.com>
2025-03-12 09:36:49 +0100

    Move to www.grandsvoisins.com

--- nginx/wagtail.nix
+++ nginx/wagtail.nix
@@ -135,13 +135,13 @@ in {
       locations."/" = {
         proxyPass = "http://localhost:8906/";
         extraConfig = nginxLocationWagtailExtraConfig + ''
-          return 302 $scheme://www.grandsvoisins.org$request_uri;
-          # if ($host != 'www.grandsvoisins.com') {
-          #   return 301 $scheme://www.grandsvoisins.com$request_uri;
-          # }
-          if ($host = 'lesgrandsvoisins.com') {
-            return 301 $scheme://www.lesgrandsvoisins.com$request_uri;
+          # return 302 $scheme://www.grandsvoisins.org$request_uri;
+          if ($host != 'www.grandsvoisins.com') {
+            return 301 $scheme://www.grandsvoisins.com$request_uri;
           }
+          # if ($host = 'lesgrandsvoisins.com') {
+          #   return 301 $scheme://www.lesgrandsvoisins.com$request_uri;
+          # }
           if ($host = 'grandsvoisins.com') {
             return 301 $scheme://www.grandsvoisins.com$request_uri;
           }
@@ -226,7 +226,7 @@ in {
         proxyPass = "http://localhost:8904/";
         # proxyPass = "http://localhost:8894/";
         extraConfig = nginxLocationWagtailExtraConfig + ''
-          # return 302 $scheme://www.grandsvoisins.com$request_uri;
+          return 302 $scheme://www.grandsvoisins.com$request_uri;
           if ($host = 'grandsvoisins.org') {
             return 301 $scheme://www.grandsvoisins.org$request_uri;
           }
