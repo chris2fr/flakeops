9cb3ea31051bbf4bbef5b82cc1659857cbd4f5c8
Chris Mann <chris@lesgtrandsvoisins.com>
2025-03-18 08:02:16 +0100

    publii.resdigita.com

--- nginx.nix
+++ nginx.nix
@@ -294,7 +294,7 @@ in
           forceSSL = true;
           locations."/".return = "302 https://mail.lesgrandsvoisins.com";
         };
-        "publi.resdigita.com" = {
+        "publii.resdigita.com" = {
           enableACME = true;
           forceSSL = true;
           root = "/var/www/publii";
