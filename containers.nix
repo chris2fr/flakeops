6472cc3b0399de1e61c63ee2f61b9c83700b6d1a
Chris Mann <chris@lesgtrandsvoisins.com>
2025-03-19 14:03:27 +0100

    No more discourse

--- containers.nix
+++ containers.nix
@@ -36,7 +36,7 @@ in
   imports = [
     ./containers/cherryldap.nix
     ./containers/discourse.nix
-    ./containers/discourseparis14cc.nix
+    # ./containers/discourseparis14cc.nix
     ./containers/key.nix
     ./containers/keycloak.nix
     ./containers/keyresdigita.nix
