0bf84fd8498ff4f09b7c2d6383bd3c953b421f66
Chris Mann <chris@lesgtrandsvoisins.com>
2025-04-02 18:33:58 +0200

    Force gvois.com

--- nginx/wagtail.nix
+++ nginx/wagtail.nix
@@ -242,8 +242,8 @@ in {
         # proxyPass = "http://localhost:8894/";
         extraConfig = nginxLocationWagtailExtraConfig + ''
           # return 302 $scheme://www.grandsvoisins.com$request_uri;
-          if ($host = 'www.gvois.com') {
-            return 301 $scheme://www.gvois.org$request_uri;
+          if ($host = 'www.gvois.org') {
+            return 301 $scheme://www.gvois.com$request_uri;
           }
           # if ($host = 'grandsvoisins.org') {
           #   return 301 $scheme://www.grandsvoisins.org$request_uri;
