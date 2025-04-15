6a201f611712ad8eaea1a518c83baa800f05f212
Chris Mann <chris@lesgtrandsvoisins.com>
2025-04-15 22:37:19 +0200

    keycloak.parisgv.com

--- nginx.nix
+++ nginx.nix
@@ -245,6 +245,33 @@ in {
             '';
           };
         };
+        "keycloak.parisgv.com" = {
+          enableACME = true;
+          forceSSL = true;
+          root = "/var/www/keycloak.parisgv.com";
+          serverAliases = [ "adminkeycloak.parisgv.com" "keycloak.parisgv.org"  "adminkeycloak.parisgv.org" ];
+          locations."/" = {
+            proxyPass = "https://192.168.116.11:14446";
+            extraConfig = ''
+              rewrite ^/$ https://keycloak.parisgv.com/realms/master/account/applications redirect;
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Host $host;
+              proxy_set_header X-Forwarded-Proto $scheme;
+              add_header Content-Security-Policy "frame-src *; frame-ancestors *; object-src *;";
+              add_header Access-Control-Allow-Credentials true;
+              proxy_ssl_certificate     /var/lib/acme/keycloak.parisgv.com/fullchain.pem;
+              proxy_ssl_certificate_key /var/lib/acme/keycloak.parisgv.com/key.pem;
+              if ($host = "keycloak.parisgv.org") {
+                return 302 $scheme://keycloak.parisgv.com$request_uri;
+              }
+              if ($host = "adminkeycloak.parisgv.org") {
+                return 302 $scheme://adminkeycloak.parisgv.com$request_uri;
+              }
+            '';
+          };
+        };
         "keycloak.gvois.com" = {
           enableACME = true;
           forceSSL = true;
