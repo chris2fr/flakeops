41f258a12a9ac522e8c0004074706285a44f8d28
Chris Mann <chris@lesgtrandsvoisins.com>
2025-04-16 08:36:31 +0200

    www.gdvox.com defined once

--- nginx/wagtail.nix
+++ nginx/wagtail.nix
@@ -477,16 +477,14 @@ in {
       ];
       enableACME = true;
       forceSSL = true;
-      # root = "/var/www/lesgrandsvoisins/";      
       root = "/var/www/coopgv/";
       locations."/" = {
-        # return =  "302 https://blog.lesgrandsvoisins.com";
         proxyPass = "http://localhost:8904/";
         extraConfig = nginxLocationWagtailExtraConfig + ''
             # return 302 $scheme://www.grandsvoisins.com$request_uri;
-            if ($host = 'www.parisgv.org') {
-              return 301 $scheme://www.parisgv.com$request_uri;
-            }
+            # if ($host = 'www.parisgv.org') {
+            #   return 301 $scheme://www.parisgv.com$request_uri;
+            # }
             rewrite ^/cms-admin/login/?$ /accounts/oidc/key-lesgrandsvoisins-com/login/?process=cms-admin/login/ redirect; 
         '';
       };
@@ -585,27 +583,6 @@ in {
       locations."/en/accounts/profile/".extraConfig = ''
         return 302 /;
       '';
-      locations."/favicon.ico" = { proxyPass = null; };
-      locations."/static" = { proxyPass = null; };
-      locations."/medias" = { proxyPass = null; };
-      locations."/.well-known" = { proxyPass = null; };
-    };
-    "www.gdvox.com" = {
-      enableACME = true;
-      forceSSL = true;
-      # root = "/var/www/lesgrandsvoisins/";      
-      root = "/var/www/coopgv/";
-      locations."/" = {
-        # return =  "302 https://blog.lesgrandsvoisins.com";
-        proxyPass = "http://localhost:8904/";
-        extraConfig = nginxLocationWagtailExtraConfig + ''
-            # return 302 $scheme://www.grandsvoisins.com$request_uri;
-            # if ($host = 'www.parisle.org') {
-            #   return 301 $scheme://www.parisle.com$request_uri;
-            # }
-            rewrite ^/cms-admin/login/?$ /accounts/oidc/key-lesgrandsvoisins-com/login/?process=cms-admin/login/ redirect; 
-        '';
-      };
       locations."/favicon.ico" = { proxyPass = null; };
       locations."/static" = { proxyPass = null; };
       locations."/medias" = { proxyPass = null; };
