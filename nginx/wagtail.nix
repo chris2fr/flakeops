cab0afb89cfe339ea67e741c0830fb3ce8c11f43
Chris Mann <chris@lesgtrandsvoisins.com>
2025-04-16 09:49:01 +0200

    admin login on gdvox

--- nginx/wagtail.nix
+++ nginx/wagtail.nix
@@ -485,7 +485,7 @@ in {
             # if ($host = 'www.parisgv.org') {
             #   return 301 $scheme://www.parisgv.com$request_uri;
             # }
-            rewrite ^/cms-admin/login/?$ /accounts/oidc/key-lesgrandsvoisins-com/login/?process=cms-admin/login/ redirect; 
+            rewrite ^/admin$ /accounts/oidc/keycloak-gdvox-com/login/?process=cms-admin/login/ redirect; 
         '';
       };
       locations."/fr/accounts/profile/".extraConfig = ''
