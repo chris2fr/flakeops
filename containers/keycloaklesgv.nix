8055da6b104c3276f0bc613f15707dd5b952971b
Chris Mann <chris@lesgtrandsvoisins.com>
2025-04-16 16:16:45 +0200

    starting install of keycloak.lesgv.com

---
+++ containers/keycloaklesgv.nix
@@ -0,0 +1,138 @@
+{ config, pkgs, lib, ... }:
+let
+in
+{
+  containers.keycloaklesgv = {
+    bindMounts = {
+      "/var/lib/acme/keycloak.lesgv.org/" = {
+        hostPath = "/var/lib/acme/keycloak.lesgv.org/";
+        isReadOnly = true;
+      };
+    };
+    autoStart = true;
+    privateNetwork = true;
+    hostAddress = "192.168.116.10";
+    localAddress = "192.168.116.11";
+    hostAddress6 = "fc00::116:10";
+    localAddress6 = "fc00::116:11";
+    config = { config, pkgs, lib, ... }: {
+      environment.systemPackages = with pkgs; [
+        ((vim_configurable.override { }).customize {
+          name = "vim";
+          vimrcConfig.customRC = ''
+            " your custom vimrc
+            set mouse=a
+            set nocompatible
+            colo torte
+            syntax on
+            set tabstop     =2
+            set softtabstop =2
+            set shiftwidth  =2
+            set expandtab
+            set autoindent
+            set smartindent
+            " ...
+          '';
+        }
+        )
+        git
+        lynx
+        openldap
+        postgresql_17
+        openssl
+      ];
+      # virtualisation.docker.enable = true;
+      system.stateVersion = "24.11";
+      nix.settings.experimental-features = "nix-command flakes";
+      networking = {
+        firewall = {
+          enable = false;
+          allowedTCPPorts = [ 443 587 14446 ];
+        };
+        useHostResolvConf = lib.mkForce false;
+      };
+      systemd.tmpfiles.rules = [
+        # "f /etc/.secret.keycloaklesgvorgdata 0660 root root"
+        "d /var/lib/acme/keycloak.lesgv.org/ 0750 acme wwwrun"
+        "d /etc/postgresql/ 0750 postgres keycloak"
+        "f /etc/.secret.keycloaklesgvorg 0660 keycloak postgres"
+      ];
+      # security.acme.acceptTerms = true;
+      users = {
+        groups = {
+          "acme" = {
+            gid = 993;
+            members = [ "acme" ];
+          };
+          "wwwrun" = {
+            gid = 54;
+            members = [ "acme" "wwwrun" "keycloak"];
+          };
+          "keycloak" = {
+            members = [ "keycloak" ];
+          };
+        };
+        users = {
+          "acme" = {
+            uid = 994;
+            group = "acme";
+          };
+          "wwwrun" = {
+            uid = 54;
+            group = "wwwrun";
+          };
+          "keycloak" = {
+            group = "keycloak";
+            isSystemUser = true;
+          };
+        };
+      };
+      # systemd.services.postgresql.postStart = "cp -a ~postgres/server.crt /run/postgresql/server.crt";
+
+      services = {
+        resolved.enable = true;
+        postgresql = {
+          package = pkgs.postgresql_17;
+          enable = true;
+          enableTCPIP = true;
+          settings = {
+            ssl = true;
+            ssl_cert_file = "/etc/postgres/server.crt";
+          };
+          ensureUsers = [{
+            name = "keycloaklesgv";
+            ensureDBOwnership = true;
+          }];
+          ensureDatabases = ["keycloaklesgv"];
+        };
+        keycloak = {
+          enable = true;
+          database = {
+            username = "keycloaklesgv";
+            # name = "keycloaklesgv";
+            name="keycloaklesgv"; # I think the database is keycloak and not key
+            # passwordFile="/etc/.secrets.key";
+            passwordFile = "/etc/.secret.keycloaklesgvorg";
+            createLocally=false;
+            host="127.0.0.1";
+            # useSSL = false;
+            caCert = "/etc/postgres/server.crt";
+          };
+          settings = {
+            https-port = 14446;
+            http-port = 14086;
+            # proxy = "passthrough";
+            # proxy = "reencrypt";
+            proxy-headers = "xforwarded";
+            hostname = "keycloak.lesgv.org";
+            # hostname-admin = "adminkeycloak.lesgv.org";
+          };
+          sslCertificate = "/var/lib/acme/keycloak.lesgv.org/fullchain.pem";
+          sslCertificateKey = "/var/lib/acme/keycloak.lesgv.org/key.pem";
+          initialAdminPassword = "lksajdflkasjlkgh5798214dskjhgfkjsahf";
+          # themes = {lesgv = (pkgs.callPackage "/etc/nixos/keycloaktheme/derivation.nix" {});};
+        };
+      };
+    };
+  };
+}
--- nginx.nix
+++ nginx.nix
@@ -245,6 +245,27 @@ in {
             '';
           };
         };
+        "keycloak.lesgv.org" = {
+          enableACME = true;
+          forceSSL = true;
+          root = "/var/www/keycloak.gvois.com";
+          serverAliases = ["adminkeycloak.lesgv.org"];
+          locations."/" = {
+            proxyPass = "https://192.168.116.11:14446";
+            extraConfig = ''
+              rewrite ^/$ https://keycloak.lesgv.org/realms/master/account/applications redirect;
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Host $host;
+              proxy_set_header X-Forwarded-Proto $scheme;
+              add_header Content-Security-Policy "frame-src *; frame-ancestors *; object-src *;";
+              add_header Access-Control-Allow-Credentials true;
+              proxy_ssl_certificate     /var/lib/acme/keycloak.lesgv.org/fullchain.pem;
+              proxy_ssl_certificate_key /var/lib/acme/keycloak.lesgv.org/key.pem;
+            '';
+          };
+        };
         "keycloak.parisgv.com" = {
           enableACME = true;
           forceSSL = true;
