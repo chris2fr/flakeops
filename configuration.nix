e9aa097587659460d8c0b05770b6ffab3f39eb91
Chris Mann <chris@lesgtrandsvoisins.com>
2025-06-09 08:38:10 +0200

    missing profile maybe

--- configuration.nix
+++ configuration.nix
@@ -106,10 +106,10 @@ in
         OAUTH_AUTHORIZATION_URL = 'https://key.lesgrandsvoisins.com/realms/master/protocol/openid-connect/auth'
         OAUTH_TOKEN_URL = 'https://key.lesgrandsvoisins.com/realms/master/protocol/openid-connect/token'
         OAUTH_USER_INFO_URL = 'https://key.lesgrandsvoisins.com/realms/master/protocol/openid-connect/userinfo'
-        OAUTH_SCOPE = ["profile","email"]
+        OAUTH_SCOPE = ["openid","profile","email"]
         OAUTH_ATTRIBUTE_MAP = {
           "email": (True, "email"),
-          "username": (True, "uid"),
+          "sub": (True, "uid"),
           "username": (False, "name"),
           "email": (False, "contact_email"),  
         }
