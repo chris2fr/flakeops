47070873ac8e3fc565ce9204f00640499b50646f
Chris Mann <chris@lesgtrandsvoisins.com>
2025-06-21 13:47:04 +0200

    Firgure out the callback on your own

--- configuration.nix
+++ configuration.nix
@@ -57,7 +57,7 @@ in
       clientID = "seafile";
       # clientSecret = "YOUR_CLIENT_SECRET";
       keyFile = "/etc/.secrets/.seafile_oauthproxy_keyfile";
-      redirectURL = "https://roses.lgv.info:41443/callback";
+      # redirectURL = "https://roses.lgv.info:41443/callback";
       # cookieSecret = "long-random-cookie-secret";  # must be 16, 24, or 32 chars
       setXauthrequest = true;
       passAccessToken = true;
