21f0981cd169949fc56e6cd743ba9c3ac9840e2e
Chris Mann <chris@lesgtrandsvoisins.com>
2025-06-21 22:19:20 +0200

    simpler httpd

--- httpd.nix
+++ httpd.nix
@@ -20,53 +20,53 @@ in
         Protocols h2 http/1.1
       '';
       adminAddr = "chris@lesgrandsvoisins.com";
-      extraModules = [ 
-        "proxy" 
-        "proxy_http" 
-        "dav" 
-        "ldap" 
-        "authnz_ldap" 
-        "alias" 
-        "ssl" 
-        "rewrite" 
-        "proxy_fcgi" 
-        "http2" 
-        "proxy_uwsgi"
-        { 
-          name = "auth_openidc"; 
-          path = "/usr/local/lib/modules/mod_auth_openidc.so"; 
-        }
-      ];
+      # extraModules = [ 
+      #   "proxy" 
+      #   "proxy_http" 
+      #   "dav" 
+      #   "ldap" 
+      #   "authnz_ldap" 
+      #   "alias" 
+      #   "ssl" 
+      #   "rewrite" 
+      #   "proxy_fcgi" 
+      #   "http2" 
+      #   "proxy_uwsgi"
+      #   { 
+      #     name = "auth_openidc"; 
+      #     path = "/usr/local/lib/modules/mod_auth_openidc.so"; 
+      #   }
+      # ];
       virtualHosts = {
         "roses.lgv.info" = {
           forceSSL = true;
           enableACME = true;
-          listen = [{port = 443; ssl=true;}];
+          # listen = [{port = 443; ssl=true;}];
           # sslServerCert = "/var/lib/acme/roses.lgv.info/fullchain.pem";
           # sslServerChain = "/var/lib/acme/roses.lgv.info/fullchain.pem";
           # sslServerKey = "/var/lib/acme/roses.lgv.info/key.pem";
           documentRoot = "/var/www/default";
-          extraConfig = ''
-            ProxyPreserveHost On
-            # ProxyVia On
-            ProxyAddHeaders On
-            OIDCProviderMetadataURL https://key.lesgrandsvoisins.com/realms/master/.well-known/openid-configuration
-            OIDCClientID seafile
-            OIDCClientSecret open("/etc/.secrets/.seafile_client_secret").read().strip()
-            OIDCRedirectURI https://roses.lgv.info/redirect_uri_from_oauth2
-            OIDCCryptoPassphrase UMU0I51HADokJraIaBSjpI89zhnGjuhv
-            <Location "/">
-              AuthType openid-connect
-              Require valid-user
-              # # ProxyPass unix:/opt/filebrowser/dbs/filebrowser/maruftuyel/filebrowser.sock|http://127.0.0.1/
-              # # ProxyPass unix:/opt/filebrowser/dbs/filebrowser/%{env:MATCH_USERNAME}/filebrowser.sock|http://filebrowser.resdigita.com/
-              # RequestHeader set FileBrowserUser %{env:OIDC_CLAIM_username}s  
-              # RequestHeader set X-Forwarded-Proto "https"
-              # RequestHeader set X-Forwarded-Port "443"
-              # RequestHeader set X-Forwarded-For "$proxy_add_x_forwarded_for"
-              # RequestHeader set Host $host
-            </Location>
-          '';
+          # extraConfig = ''
+          #   ProxyPreserveHost On
+          #   # ProxyVia On
+          #   ProxyAddHeaders On
+          #   OIDCProviderMetadataURL https://key.lesgrandsvoisins.com/realms/master/.well-known/openid-configuration
+          #   OIDCClientID seafile
+          #   OIDCClientSecret open("/etc/.secrets/.seafile_client_secret").read().strip()
+          #   OIDCRedirectURI https://roses.lgv.info/redirect_uri_from_oauth2
+          #   OIDCCryptoPassphrase UMU0I51HADokJraIaBSjpI89zhnGjuhv
+          #   <Location "/">
+          #     AuthType openid-connect
+          #     Require valid-user
+          #     # # ProxyPass unix:/opt/filebrowser/dbs/filebrowser/maruftuyel/filebrowser.sock|http://127.0.0.1/
+          #     # # ProxyPass unix:/opt/filebrowser/dbs/filebrowser/%{env:MATCH_USERNAME}/filebrowser.sock|http://filebrowser.resdigita.com/
+          #     # RequestHeader set FileBrowserUser %{env:OIDC_CLAIM_username}s  
+          #     # RequestHeader set X-Forwarded-Proto "https"
+          #     # RequestHeader set X-Forwarded-Port "443"
+          #     # RequestHeader set X-Forwarded-For "$proxy_add_x_forwarded_for"
+          #     # RequestHeader set Host $host
+          #   </Location>
+          # '';
         };
       };
     };
