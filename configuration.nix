5c8bac3edfc7e7118bd2a4c5d3d2bca8055f3973
Chris Mann <chris@lesgtrandsvoisins.com>
2025-06-23 16:23:21 +0200

    Import location

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
 let 
-  oidcSeafileSecret = import ./vars/oidc-seafile-secret.nix;
+  oidcSeafileSecret = import ./secrets/oidc-seafile-secret.nix;
 in
 {
   nix.settings.experimental-features = "nix-command flakes";
