3d648ea6125d8e5e0c055962b58c3a178cde09b7
Chris Mann <chris@lesgtrandsvoisins.com>
2025-06-24 08:53:53 +0200

    Full nginx integration

--- configuration.nix
+++ configuration.nix
@@ -147,7 +147,7 @@ in
           locations = {
             "/protected" = {
               extraConfig = ''
-
+                          
                 auth_request /oauth2/auth;
                 error_page 401 =403 /oauth2/sign_in;
 
@@ -157,19 +157,36 @@ in
                 auth_request_set $email  $upstream_http_x_auth_request_email;
                 proxy_set_header X-User  $user;
                 proxy_set_header X-Email $email;
-                
-                # Protect this location using the auth_request
-                # auth_request /sso-auth;
 
-                # ## Optionally set a header to pass through the username
-                # #auth_request_set $username $upstream_http_x_username;
-                # #proxy_set_header X-User $username;
+                # if you enabled --pass-access-token, this will pass the token to the backend
+                auth_request_set $token  $upstream_http_x_auth_request_access_token;
+                proxy_set_header X-Access-Token $token;
 
-                # # Automatically renew SSO cookie on request
-                # auth_request_set $cookie $upstream_http_set_cookie;
-                # add_header Set-Cookie $cookie;
+                # if you enabled --cookie-refresh, this is needed for it to work with auth_request
+                auth_request_set $auth_cookie $upstream_http_set_cookie;
+                add_header Set-Cookie $auth_cookie;
 
-                # proxy_pass http://127.0.0.1:1720/;
+                # When using the --set-authorization-header flag, some provider's cookies can exceed the 4kb
+                # limit and so the OAuth2 Proxy splits these into multiple parts.
+                # Nginx normally only copies the first `Set-Cookie` header from the auth_request to the response,
+                # so if your cookies are larger than 4kb, you will need to extract additional cookies manually.
+                auth_request_set $auth_cookie_name_upstream_1 $upstream_cookie_auth_cookie_name_1;
+
+                # Extract the Cookie attributes from the first Set-Cookie header and append them
+                # to the second part ($upstream_cookie_* variables only contain the raw cookie content)
+                if ($auth_cookie ~* "(; .*)") {
+                    set $auth_cookie_name_0 $auth_cookie;
+                    set $auth_cookie_name_1 "auth_cookie_name_1=$auth_cookie_name_upstream_1$1";
+                }
+
+                # Send both Set-Cookie headers now if there was a second part
+                if ($auth_cookie_name_upstream_1) {
+                    add_header Set-Cookie $auth_cookie_name_0;
+                    add_header Set-Cookie $auth_cookie_name_1;
+                }
+
+                # proxy_pass http://backend/;
+                # or "root /path/to/site;" or "fastcgi_pass ..." etc
               '';
             };
             "/oauth2/" = {
