6ede427b77672b58bf7726cf9ae3ce7d06f1dd47
Chris Mann <chris@lesgtrandsvoisins.com>
2025-06-24 09:01:17 +0200

    Full nginx integration

--- configuration.nix
+++ configuration.nix
@@ -145,7 +145,7 @@ in
           #   auth_request /sso-auth;
           # '';
           locations = {
-            "/protected" = {
+            "/" = {
               extraConfig = ''
                           
                 auth_request /oauth2/auth;
@@ -347,7 +347,7 @@ in
         cookie-secret="NgbKPVOqtJn5bipSRGuR22BwasVS1J5u";
         cookie-secure="false";
         email-domain="*" ;
-        http-address=":4180";
+        http-address="127.0.0.1:4180";
         https-address=":41443";
         insecure-oidc-allow-unverified-email="true" ;
         oidc-issuer-url="https://key.lesgrandsvoisins.com/realms/master";
--- workshop/oquth2-proxy.sh
+++ workshop/oquth2-proxy.sh
@@ -3,6 +3,37 @@ sudo -u oauth2-proxy /nix/store/nw5fzppysbf6qq9kl792aplvyjw2rpih-oauth2-proxy-7.
 --client-id='seafile'  \
 --client-secret-file='/etc/.secrets/.seafile_oauthproxy_keyfile' \
 --code-challenge-method='S256'  \
+--cookie-csrf-expire='5m' \
+--cookie-domain='roses.lgv.info'  \
+--cookie-expire='168h0m0s' \
+--cookie-httponly=true \
+--cookie-name='_oauth2_proxy_roses_lgv_info'  \
+--cookie-refresh="5m" \
+--cookie-samesite="none" \
+--cookie-secret='NgbKPVOqtJn5bipSRGuR22BwasVS1J5u' \
+--cookie-secure="false" \
+--email-domain='*'  \
+--http-address='127.0.0.1:4180' \
+--insecure-oidc-allow-unverified-email='true'  \
+--oidc-issuer-url='https://key.lesgrandsvoisins.com/realms/master' \
+--pass-host-header=true  \
+--provider='keycloak-oidc' \
+--proxy-prefix='/oauth2'  \
+--redirect-url='https://roses.lgv.info/oauth2/callback' \
+--request-logging="true" \
+--reverse-proxy="true" \
+--session-store-type="cookie" \
+--set-xauthrequest="true" \
+--skip-provider-button="false" \
+--upstream='file:///var/www/default'  
+
+
+
+sudo -u oauth2-proxy /nix/store/nw5fzppysbf6qq9kl792aplvyjw2rpih-oauth2-proxy-7.8.2/bin/oauth2-proxy \
+--approval-prompt='force'  \
+--client-id='seafile'  \
+--client-secret-file='/etc/.secrets/.seafile_oauthproxy_keyfile' \
+--code-challenge-method='S256'  \
 --cookie-csrf-expire=5m \
 --cookie-csrf-per-request='true' \
 --cookie-domain='roses.lgv.info'  \
@@ -14,7 +45,7 @@ sudo -u oauth2-proxy /nix/store/nw5fzppysbf6qq9kl792aplvyjw2rpih-oauth2-proxy-7.
 --cookie-secret='NgbKPVOqtJn5bipSRGuR22BwasVS1J5u' \
 --cookie-secure=false \
 --email-domain='*'  \
---http-address='0.0.0.0:8082' \
+--http-address='127.0.0.1:4180' \
 --https-address=':41443' \
 --insecure-oidc-allow-unverified-email='true'  \
 --oidc-issuer-url='https://key.lesgrandsvoisins.com/realms/master' \
