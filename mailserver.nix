cdef3b7e1a7446781fec4954925cc5201373baed
Chris Mann <chris@lesgtrandsvoisins.com>
2025-06-25 15:25:22 +0200

    Redirection Merouane

--- mailserver.nix
+++ mailserver.nix
@@ -64,6 +64,7 @@ in
   ];
   services = {
     postfix.virtual = ''
+      merouane@lesgrandsvoisins.com kachamarou2020@gmail.com
       donation@lesgrandsvoisins.com chris@lesgrandsvoisins.com
       contact@afriquepartenaires.org claudette@afriquepartenaires.org
       contact@afriquepartenaires.org renel@afriquepartenaires.org
