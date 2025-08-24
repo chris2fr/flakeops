1cd015802e6b9fcdada059fddb283fbbe82f2f9b
Chris Mann <chris@lesgtrandsvoisins.com>
2025-08-24 10:31:48 +0200

    AppArmor kills LXD

--- servers.nix
+++ servers.nix
@@ -165,7 +165,9 @@ in
   #     nodejs_18
   #   ];
   # };
-  virtualisation.lxd.enable = true;
+  # virtualisation.lxd.enable = true;
+  # chris2fr 2025-08-24 chris2fr
+  virtualisation.lxd.enable = false;
   # virtualisation.lxc.enable = true;
   virtualisation.lxc.lxcfs.enable = true;
 }
--- services/syncthing.nix
+++ services/syncthing.nix
@@ -1,33 +1,32 @@
 { config, pkgs, lib, ... }:
 let
 in {
-  # mannchri chris2fr 2025-08-24
-  # services.syncthing = {
-  #   enable = true;
-  #   openDefaultPorts = true;
-  #   # overrideFolders = true;
-  #   overrideDevices = true;
-  #   settings = {
-  #     devices = {
-  #       "mannchrilenovoslim7" = {
-  #         id = "VJKOQSN-AC3YKXV-AV4N74C-MH7HZ4R-GBTAGOV-SETMPBT-GCKJC5M-G6XSVQL";
-  #         autoAcceptFolders = true;
-  #       };
-  #       "mannchriphone" = {
-  #         id = "SUJCVUC-XXVY326-42GP5IU-UO6RMEJ-2IHAXEL-KBA4YPU-BQFQMYN-YG66ZQZ";
-  #         autoAcceptFolders = true;
-  #       };
-  #     };
-  #     folders = {
-  #       "LogSeqMann" = {
-  #         enable = true;
-  #         id = "LogSeqMann";
-  #         label = "LogSeqMann";
-  #         path = "/var/lib/syncthing/LogSeqMann";
-  #         devices = [ "mannchrilenovoslim7" "mannchriphone" ];
-  #         ignorePerms = false; # By default, Syncthing doesn't sync file permissions. This line enables it for this folder.
-  #       };
-  #     };
-  #   };
-  # };
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+    # overrideFolders = true;
+    overrideDevices = true;
+    settings = {
+      devices = {
+        "mannchrilenovoslim7" = {
+          id = "VJKOQSN-AC3YKXV-AV4N74C-MH7HZ4R-GBTAGOV-SETMPBT-GCKJC5M-G6XSVQL";
+          autoAcceptFolders = true;
+        };
+        "mannchriphone" = {
+          id = "SUJCVUC-XXVY326-42GP5IU-UO6RMEJ-2IHAXEL-KBA4YPU-BQFQMYN-YG66ZQZ";
+          autoAcceptFolders = true;
+        };
+      };
+      folders = {
+        "LogSeqMann" = {
+          enable = true;
+          id = "LogSeqMann";
+          label = "LogSeqMann";
+          path = "/var/lib/syncthing/LogSeqMann";
+          devices = [ "mannchrilenovoslim7" "mannchriphone" ];
+          ignorePerms = false; # By default, Syncthing doesn't sync file permissions. This line enables it for this folder.
+        };
+      };
+    };
+  };
 }
