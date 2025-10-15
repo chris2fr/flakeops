03d86d63768c05cbbcbabfedfbbd61b42e5d454c
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-15 12:37:06 +0200

    Added two domains for quartz

--- nginx/static.nix
+++ nginx/static.nix
@@ -15,6 +15,8 @@ in
       serverAliases = [
         "quartz.gv.coop"
         "quartz.lesgv.org"
+        "quartz.gdvoisins.com"
+        "quartz.l14s.com"
       ];
       enableACME = true;
       forceSSL = true;
