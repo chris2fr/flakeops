278ccb98d4714e6be49c0f0c83c455006dc7ae11
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-19 16:32:50 +0200

    ve-filestash

--- networking.nix
+++ networking.nix
@@ -14,7 +14,7 @@ in {
     # };
     firewall = {
       enable = true;
-      trustedInterfaces = ["lo" "eno1" "eno2" "enp3s0f0"];
+      trustedInterfaces = ["lo" "eno1" "eno2" "enp3s0f0" "ve-filestash"];
       # interfaces."eno1" = {
       #   allowedTCPPorts = [22 80 443];
       # };
