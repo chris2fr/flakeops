d480673ebc8acc20250e11736f751f7e323f1acb
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-19 16:36:55 +0200

    ve-filestash" is already a natted interface

--- networking.nix
+++ networking.nix
@@ -14,7 +14,8 @@ in {
     # };
     firewall = {
       enable = true;
-      trustedInterfaces = ["lo" "eno1" "eno2" "enp3s0f0" "ve-filestash"];
+      # trustedInterfaces = ["lo" "eno1" "eno2" "enp3s0f0" "ve-filestash"];
+      trustedInterfaces = ["lo" "eno1" "eno2" "enp3s0f0" ];
       # interfaces."eno1" = {
       #   allowedTCPPorts = [22 80 443];
       # };
