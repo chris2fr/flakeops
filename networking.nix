6c93c72311eac26813c463244aad8c475d9b8c07
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-19 16:36:02 +0200

    Use "ve-*" when using nftables instead of iptables

--- networking.nix
+++ networking.nix
@@ -23,8 +23,8 @@ in {
     enableIPv6 = true;
     nat = {
       enable = true;
-      # # Use "ve-*" when using nftables instead of iptables
-      internalInterfaces = ["ve-+"];
+      # Use "ve-*" when using nftables instead of iptables
+      internalInterfaces = ["ve-*"];
       externalInterface = "eno1";
       # Lazy IPv6 connectivity for the container
       enableIPv6 = true;
