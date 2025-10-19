aed05796afedcc32fb4089eada51fb5faf4032eb
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-19 16:14:56 +0200

    No FIrewal l but a somple bridge

--- networking.nix
+++ networking.nix
@@ -4,16 +4,21 @@ let
   allowedUDPPorts = [ 53 67 68 123 111 2049 4000 4001 4002 20048 ];
 in {
   networking = {
-    nftables = {
-      enable = true;
+    # nftables = {
+    #   enable = true;
+    # };
+    bridges = {
+      br0 = {
+        interfaces = [];
+      };
     };
-    firewall = {
-      enable = true;
-      trustedInterfaces = ["lo" "eno1" "eno2" "enp3s0f0"];
-      # interfaces."eno1" = {
-      #   allowedTCPPorts = [22 80 443];
-      # };
-    };
+    # firewall = {
+    #   enable = true;
+    #   trustedInterfaces = ["lo" "eno1" "eno2" "enp3s0f0"];
+    #   # interfaces."eno1" = {
+    #   #   allowedTCPPorts = [22 80 443];
+    #   # };
+    # };
     hostName = "rosest330"; 
     enableIPv6 = true;
     # nat = {
