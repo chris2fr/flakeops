0dd8f6829542f2efb4caf920291adba478bc0e1a
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-19 15:34:26 +0200

    macvlans = ["eno1"];

--- containers.nix
+++ containers.nix
@@ -4,12 +4,13 @@ in
 {
   containers.filestash = {
     autoStart = true;
-    # privateNetwork = true;
+    privateNetwork = true;
+    macvlans = ["eno1"];
     # hostBridge = "br-erdock";
-    hostAddress = "192.168.1.100";
-    localAddress = "192.168.1.111";
-    hostAddress6 = "fe80::1298:36ff:fea0:2131";
-    localAddress6 = "fe80::1298:36ff:fea0:0111";
+    # hostAddress = "192.168.1.100";
+    # localAddress = "192.168.1.111";
+    # hostAddress6 = "fe80::1298:36ff:fea0:2131";
+    # localAddress6 = "fe80::1298:36ff:fea0:0111";
     interfaces = ["eno1"];
     # bindMounts = {};
     config = { config, pkgs, ... }: {
--- networking.nix
+++ networking.nix
@@ -24,18 +24,18 @@ in {
     #   # Lazy IPv6 connectivity for the container
     #   enableIPv6 = true;
     # };
-    interfaces."tuncontain" = {
-      useDHCP = false;
-      virtual = true;
-      name = "tuncontain";
-      ipv4.addresses = [{
-        address = "192.168.100.11";
-        prefixLength = 24;
-      }];
-      ipv6.addresses = [{
-        address = "fc00::2";
-        prefixLength = 96;
-      }];
-    };
+    # interfaces."tuncontain" = {
+    #   useDHCP = false;
+    #   virtual = true;
+    #   name = "tuncontain";
+    #   ipv4.addresses = [{
+    #     address = "192.168.100.11";
+    #     prefixLength = 24;
+    #   }];
+    #   ipv6.addresses = [{
+    #     address = "fc00::2";
+    #     prefixLength = 96;
+    #   }];
+    # };
   };
 }
