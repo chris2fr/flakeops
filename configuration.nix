ffce8cef127eefd0f0a91baaa7bf4c504d28abae
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-19 17:38:21 +0200

    FIlestash as a service

--- configuration.nix
+++ configuration.nix
@@ -88,6 +88,15 @@ in
   # };
 
   services = {
+    filestash = {
+      enable = true;
+      # optionally customize configuration
+      settings = {
+        public_url = "https://roses.lgv.info";
+        data_dir = "/var/lib/filestash";
+        port = 8334;
+      };
+    };
     nginx = {
       enable = true;
       clientMaxBodySize = "10G";
@@ -118,6 +127,22 @@ in
         };
       };
       virtualHosts = {
+        "roses.lgv.info" = {
+          forceSSL = true;
+          enableACME = true;
+          root = "/var/www/default";
+          locations."/" = {
+            proxyPass = "http://localhost:8334";
+            recommendedProxySettings = true;
+            extraConfig = ''
+                proxy_ssl_verify off;
+                proxy_set_header Host $host;
+                # Maybe
+                proxy_pass_request_body off;
+                proxy_set_header Content-Length "";
+            '';
+          };
+        };
         # "vouch.lgv.info" = {
         #   forceSSL = true;
         #   enableACME = true;
@@ -137,99 +162,99 @@ in
         #     '';
         #   };
         # };
-        "roses.lgv.info" = {
-          forceSSL = true;
-          enableACME = true;
-          root = "/var/www/default";
-          # extraConfig = ''
-          #   auth_request /sso-auth;
-          # '';
-          locations = {
-            "/" = {
-              extraConfig = ''
+        # "roses.lgv.info" = {
+        #   forceSSL = true;
+        #   enableACME = true;
+        #   root = "/var/www/default";
+        #   # extraConfig = ''
+        #   #   auth_request /sso-auth;
+        #   # '';
+        #   locations = {
+        #     "/" = {
+        #       extraConfig = ''
                           
-                auth_request /oauth2/auth;
-                error_page 401 =403 /oauth2/sign_in;
+        #         auth_request /oauth2/auth;
+        #         error_page 401 =403 /oauth2/sign_in;
 
-                # pass information via X-User and X-Email headers to backend,
-                # requires running with --set-xauthrequest flag
-                auth_request_set $user   $upstream_http_x_auth_request_user;
-                auth_request_set $email  $upstream_http_x_auth_request_email;
-                proxy_set_header X-User  $user;
-                proxy_set_header X-Email $email;
+        #         # pass information via X-User and X-Email headers to backend,
+        #         # requires running with --set-xauthrequest flag
+        #         auth_request_set $user   $upstream_http_x_auth_request_user;
+        #         auth_request_set $email  $upstream_http_x_auth_request_email;
+        #         proxy_set_header X-User  $user;
+        #         proxy_set_header X-Email $email;
 
-                # if you enabled --pass-access-token, this will pass the token to the backend
-                auth_request_set $token  $upstream_http_x_auth_request_access_token;
-                proxy_set_header X-Access-Token $token;
+        #         # if you enabled --pass-access-token, this will pass the token to the backend
+        #         auth_request_set $token  $upstream_http_x_auth_request_access_token;
+        #         proxy_set_header X-Access-Token $token;
 
-                # if you enabled --cookie-refresh, this is needed for it to work with auth_request
-                auth_request_set $auth_cookie $upstream_http_set_cookie;
-                add_header Set-Cookie $auth_cookie;
+        #         # if you enabled --cookie-refresh, this is needed for it to work with auth_request
+        #         auth_request_set $auth_cookie $upstream_http_set_cookie;
+        #         add_header Set-Cookie $auth_cookie;
 
-                # When using the --set-authorization-header flag, some provider's cookies can exceed the 4kb
-                # limit and so the OAuth2 Proxy splits these into multiple parts.
-                # Nginx normally only copies the first `Set-Cookie` header from the auth_request to the response,
-                # so if your cookies are larger than 4kb, you will need to extract additional cookies manually.
-                auth_request_set $auth_cookie_name_upstream_1 $upstream_cookie_auth_cookie_name_1;
+        #         # When using the --set-authorization-header flag, some provider's cookies can exceed the 4kb
+        #         # limit and so the OAuth2 Proxy splits these into multiple parts.
+        #         # Nginx normally only copies the first `Set-Cookie` header from the auth_request to the response,
+        #         # so if your cookies are larger than 4kb, you will need to extract additional cookies manually.
+        #         auth_request_set $auth_cookie_name_upstream_1 $upstream_cookie_auth_cookie_name_1;
 
-                # Extract the Cookie attributes from the first Set-Cookie header and append them
-                # to the second part ($upstream_cookie_* variables only contain the raw cookie content)
-                if ($auth_cookie ~* "(; .*)") {
-                    set $auth_cookie_name_0 $auth_cookie;
-                    set $auth_cookie_name_1 "auth_cookie_name_1=$auth_cookie_name_upstream_1$1";
-                }
+        #         # Extract the Cookie attributes from the first Set-Cookie header and append them
+        #         # to the second part ($upstream_cookie_* variables only contain the raw cookie content)
+        #         if ($auth_cookie ~* "(; .*)") {
+        #             set $auth_cookie_name_0 $auth_cookie;
+        #             set $auth_cookie_name_1 "auth_cookie_name_1=$auth_cookie_name_upstream_1$1";
+        #         }
 
-                # Send both Set-Cookie headers now if there was a second part
-                if ($auth_cookie_name_upstream_1) {
-                    add_header Set-Cookie $auth_cookie_name_0;
-                    add_header Set-Cookie $auth_cookie_name_1;
-                }
+        #         # Send both Set-Cookie headers now if there was a second part
+        #         if ($auth_cookie_name_upstream_1) {
+        #             add_header Set-Cookie $auth_cookie_name_0;
+        #             add_header Set-Cookie $auth_cookie_name_1;
+        #         }
 
-                # proxy_pass http://backend/;
-                # or "root /path/to/site;" or "fastcgi_pass ..." etc
-              '';
-            };
-            "/oauth2/" = {
-              extraConfig = ''
-                proxy_pass       http://127.0.0.1:4180;
-                proxy_set_header Host                    $host;
-                proxy_set_header X-Real-IP               $remote_addr;
-                proxy_set_header X-Auth-Request-Redirect $request_uri;
-                # or, if you are handling multiple domains:
-                # proxy_set_header X-Auth-Request-Redirect $scheme://$host$request_uri;
-              '';
-            };
-            "/oauth2/auth" = {
-              extraConfig = ''
-                proxy_pass       http://127.0.0.1:4180;
-                proxy_set_header Host             $host;
-                proxy_set_header X-Real-IP        $remote_addr;
-                proxy_set_header X-Forwarded-Uri  $request_uri;
-                # nginx auth_request includes headers but not body
-                proxy_set_header Content-Length   "";
-                proxy_pass_request_body           off;
-              '';
-            };
-            # "/sso-auth" = {
-            #   extraConfig = ''
-            #     # # Do not allow requests from outside
-            #     # internal;
-            #     # Access /auth endpoint to query login state
-            #     proxy_pass http://127.0.0.1:8082/auth;
-            #     # Do not forward the request body (nginx-sso does not care about it)
-            #     proxy_pass_request_body off;
-            #     proxy_set_header Content-Length "";
-            #     # Set custom information for ACL matching: Each one is available as
-            #     # a field for matching: X-Host = x-host, ...
-            #     proxy_set_header X-Origin-URI $request_uri;
-            #     proxy_set_header X-Host $http_host;
-            #     proxy_set_header X-Real-IP $remote_addr;
-            #     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            #     proxy_set_header X-Forwarded-Proto $scheme;
-            #     proxy_set_header X-Application "kibana";
-            #   '';
-            # };
-          };
+        #         # proxy_pass http://backend/;
+        #         # or "root /path/to/site;" or "fastcgi_pass ..." etc
+        #       '';
+        #     };
+        #     "/oauth2/" = {
+        #       extraConfig = ''
+        #         proxy_pass       http://127.0.0.1:4180;
+        #         proxy_set_header Host                    $host;
+        #         proxy_set_header X-Real-IP               $remote_addr;
+        #         proxy_set_header X-Auth-Request-Redirect $request_uri;
+        #         # or, if you are handling multiple domains:
+        #         # proxy_set_header X-Auth-Request-Redirect $scheme://$host$request_uri;
+        #       '';
+        #     };
+        #     "/oauth2/auth" = {
+        #       extraConfig = ''
+        #         proxy_pass       http://127.0.0.1:4180;
+        #         proxy_set_header Host             $host;
+        #         proxy_set_header X-Real-IP        $remote_addr;
+        #         proxy_set_header X-Forwarded-Uri  $request_uri;
+        #         # nginx auth_request includes headers but not body
+        #         proxy_set_header Content-Length   "";
+        #         proxy_pass_request_body           off;
+        #       '';
+        #     };
+        #     # "/sso-auth" = {
+        #     #   extraConfig = ''
+        #     #     # # Do not allow requests from outside
+        #     #     # internal;
+        #     #     # Access /auth endpoint to query login state
+        #     #     proxy_pass http://127.0.0.1:8082/auth;
+        #     #     # Do not forward the request body (nginx-sso does not care about it)
+        #     #     proxy_pass_request_body off;
+        #     #     proxy_set_header Content-Length "";
+        #     #     # Set custom information for ACL matching: Each one is available as
+        #     #     # a field for matching: X-Host = x-host, ...
+        #     #     proxy_set_header X-Origin-URI $request_uri;
+        #     #     proxy_set_header X-Host $http_host;
+        #     #     proxy_set_header X-Real-IP $remote_addr;
+        #     #     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        #     #     proxy_set_header X-Forwarded-Proto $scheme;
+        #     #     proxy_set_header X-Application "kibana";
+        #     #   '';
+        #     # };
+          # };
           # extraConfig = ''
           #   auth_request /validate;
           #   error_page 401 = @error401;
@@ -299,7 +324,7 @@ in
             #   '';
             # };
           # };
-        };
+        # };
       };
     };
 
