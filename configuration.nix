7454f2b549a911570cd6d72fe96cee5b24004945
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-19 17:54:46 +0200

    enable filestash

--- configuration.nix
+++ configuration.nix
@@ -89,15 +89,15 @@ in
   # };
 
   services = {
-    # filestash = {
-    #   enable = true;
-    #   # optionally customize configuration
-    #   settings = {
-    #     public_url = "https://roses.lgv.info";
-    #     data_dir = "/var/lib/filestash";
-    #     port = 8334;
-    #   };
-    # };
+    filestash = {
+      enable = true;
+      # # optionally customize configuration
+      # settings = {
+      #   public_url = "https://roses.lgv.info";
+      #   data_dir = "/var/lib/filestash";
+      #   port = 8334;
+      # };
+    };
     nginx = {
       enable = true;
       clientMaxBodySize = "10G";
