6f895c6ecc3844ed790406c7691a8571f24c2c40
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-21 16:30:22 +0200

    Proxy

--- configuration.nix
+++ configuration.nix
@@ -198,11 +198,12 @@ in
               '';
             };
             "/validate" = {
+              proxyPass = "http://127.0.0.1:30746/validate";
               extraConfig = ''
                 # forward the /validate request to Vouch Proxy
                 # proxy_pass http://127.0.0.1:30746/validate;
                 # # be sure to pass the original host header
-                # proxy_set_header Host $http_host;
+                proxy_set_header Host $http_host;
 
                 # # Vouch Proxy only acts on the request headers
                 # proxy_pass_request_body off;
