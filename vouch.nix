c2805a3a915073a3046042380cfb1a1394f36f58
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-22 11:39:30 +0200

    listen = "0.0.0.0";

--- vouch.nix
+++ vouch.nix
@@ -4,7 +4,7 @@
       vouchConfig = {
         vouch = {
           # testing = true;
-          listen = "127.0.0.1";
+          listen = "0.0.0.0";
           port = 30746;
           testing = true;
 
