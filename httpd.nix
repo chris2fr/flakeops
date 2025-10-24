09777a582806099e6e2e08b36861db69df4b76ae
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-24 14:08:41 +0200

    Remote User in Rewrite

--- httpd.nix
+++ httpd.nix
@@ -72,16 +72,16 @@ in
               MellonUser "username"
 
               # RequestHeader set "X-Forwarded-Proto" expr=%{REQUEST_SCHEME}
-              RequestHeader set X-REMOTE-USER %{REMOTE_USER}s
+              # RequestHeader set X-REMOTE-USER %{REMOTE_USER}s
 
               # RequestHeader set REMOTE_USER "chris"
               # RequestHeader set X-REMOTE-USER "pauline"
 
 
-              # RewriteEngine on
-              # RewriteCond %{REMOTE_USER} (.*)
-              # RewriteRule .* - [E=X_REMOTE_USER:%1]
-              # RequestHeader set REMOTE_USER %{X_REMOTE_USER}e
+              RewriteEngine on
+              RewriteCond %{REMOTE_USER} (.*)
+              RewriteRule .* - [E=X_REMOTE_USER:%1]
+              RequestHeader set REMOTE_USER %{X_REMOTE_USER}e
 
 
               # RewriteEngine On
