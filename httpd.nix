58a28de49fcdbbc2f25e890925340bde7ba36eaa
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-24 15:03:18 +0200

    Not the correct strategy

--- httpd.nix
+++ httpd.nix
@@ -56,12 +56,12 @@ in
               SSLProxyMachineCertificatePath /var/lib/copyparty/ssl/
               SSLProxyEngine on
           '';
-          locations."/public/" = {
-              extraConfig = ''
-                Satisfy Any
-                Allow from all
-              '';
-          };
+          # locations."/public/" = {
+          #     extraConfig = ''
+          #       Satisfy Any
+          #       Allow from all
+          #     '';
+          # };
           locations."/" = {
             proxyPass = "https://[::1]:3923/";
             extraConfig = ''
