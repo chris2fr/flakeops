5ce1306ed0b1582d2c10ffcd34bb5590b849dc31
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-26 18:16:57 +0100

    https://login.gdvoisins.com/auth

--- nginx.nix
+++ nginx.nix
@@ -69,7 +69,7 @@ let
         return 302 https://login.gdvoisins.com/logout?go=$scheme://$host/;
       '';
       "/sso-auth" = {
-        proxyPass = "https://login.gdvoisins.com";
+        proxyPass = "https://login.gdvoisins.com/auth";
         # proxyPass = "http://[::1]:8082/auth";
         # proxyPass = "http://[::1]:8082/auth";
         extraConfig = ''
