fe0e58d3bb649d48a7fd553f6136c743af3aca03
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-26 13:27:36 +0100

    login.gdvoisins.com

--- nginx.nix
+++ nginx.nix
@@ -93,6 +93,8 @@ in {
 
         };
         "login.gdvoisins.com".locations."/" = {
+          forceSSL = true;
+          enableACME = true;
           proxyPass = "http://127.0.0.1:8082/";
         };
         "nsso.gdvoisins.com" = {
