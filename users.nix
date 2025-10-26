b42285acd3d3708f210ce79722aa566c51f10a34
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-26 21:17:52 +0100

    acme

--- users.nix
+++ users.nix
@@ -9,7 +9,7 @@ in {
     mannchri = {
       isNormalUser = true;
       openssh.authorizedKeys.keys = mannchriRsaPublic;
-      extraGroups = [ "wheel" "users" "docker" "wwwrun" ];
+      extraGroups = [ "wheel" "users" "docker" "wwwrun" "acme" ];
       # extraGroups = [ "wheel" "nginx" "users" "docker" ];
     };
     admin = {
