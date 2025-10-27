c6317270e4baaaff842f9b51ba5bb6aca5cedd1d
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-27 05:40:17 +0100

    wrong route

--- containers/key.nix
+++ containers/key.nix
@@ -218,7 +218,7 @@ in
             # http-port = 14080;            
             https-port = 443;
             http-port = 80;
-            # http-host = "[2a01:4f8:241:4faa::11]";
+            http-host = "[2a01:4f8:241:4faa::11]";
 
             # proxy = "passthrough";
             # proxy = "reencrypt";
