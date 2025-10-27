f674839428879c2ac37eaac9141f99e3612fdee6
Chris Mann <chris@lesgtrandsvoisins.com>
2025-10-27 04:30:40 +0100

    Dataxase back networking

--- networking.nix
+++ networking.nix
@@ -28,6 +28,14 @@ in
         { address = "2a01:4f8:241:4faa::10"; prefixLength = 125; }
         { address = "2a01:4f8:241:4faa::443"; prefixLength = 120; }
       ];
+      routes = [
+              {
+          address = "2a01:4f8:241:4faa::11";
+          prefixLength = 125;
+          via = "2a01:4f8:241:4faa::12";
+          type = "unicast";
+        }
+      ];
     };
     nat = {
       forwardPorts = [
