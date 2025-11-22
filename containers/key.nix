6925346f71219ecb1c36239e33769b6190062c92
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-23 00:00:28 +0100

    wikijs

--- containers/key.nix
+++ containers/key.nix
@@ -227,8 +227,8 @@ in
             # proxy-headers = "xforwarded";
             hostname = "key.lesgrandsvoisins.com";
             # hostname-admin = "adminkey.lesgrandsvoisins.com";
-            jdbc-params = "sslmode=require&sslcert=/etc/postgresql/root.crt";
-            db-url-properties = "sslmode=require&sslcert=/etc/postgresql/root.crt";
+            # jdbc-params = "sslmode=require&sslcert=/etc/postgresql/root.crt";
+            # db-url-properties = "sslmode=require&sslcert=/etc/postgresql/root.crt";
           };
           sslCertificate = "/var/lib/acme/key.lesgrandsvoisins.com/fullchain.pem";
           sslCertificateKey = "/var/lib/acme/key.lesgrandsvoisins.com/key.pem";
