b64b1eba30cd5a57a919796ea1617aedf4537510
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-22 23:38:03 +0100

    wikijs

--- containers/key.nix
+++ containers/key.nix
@@ -214,12 +214,12 @@ in
             # caCert = "/etc/postgresql/root.crt";
           };
           settings = {
-            # https-port = 14443;
-            # http-port = 14080;            
-            https-port = 443;
-            http-port = 80;
+            https-port = 14443;
+            http-port = 14080;            
+            # https-port = 443;
+            # http-port = 80;
             # http-host = "192.168.105.11";
-            http-host = "2a01:4f8:241:4faa::10";
+            http-host = "2a01:4f8:241:4faa::";
             proxy-protocol-enabled = true;
             # proxy = "passthrough";
             # proxy = "reencrypt";
