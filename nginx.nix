67a8f4b22271c7119cf9f746aa4655ee71b9a91b
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-23 14:03:40 +0100

    certwarden

--- nginx.nix
+++ nginx.nix
@@ -92,7 +92,7 @@ in
               proxy_set_header    X-Forwarded-For $proxy_add_x_forwarded_for;
               proxy_set_header    X-Forwarded-Host $host;
               proxy_set_header    X-Forwarded-Proto $scheme
-              rewrite ^/$  https://$host$request_uri;
+              # rewrite ^/$  https://$host$request_uri;
             '';
 
         };
