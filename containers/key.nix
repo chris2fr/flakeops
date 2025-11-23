24beb645fbac1d2d2641277b4f201c8979e5f7a1
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-23 02:07:44 +0100

    proxy_protocol back on

--- containers/key.nix
+++ containers/key.nix
@@ -198,7 +198,7 @@ in
         resolved.enable = true;
         postgresql.package = pkgs.postgresql_15;
         # postgresql.settings.port = 5433;
-        # postgresql.enableTCPIP = true;
+        postgresql.enableTCPIP = false;
         keycloak = {
           enable = true;
           database = {
@@ -209,9 +209,11 @@ in
             # createLocally=false;
             # createLocally=true;
             # host="localhost";
-            useSSL = true;
+            host="/run/postgresql/";
+            useSSL = false;
+            # useSSL = true;
             # port = 5433;
-            caCert = "/etc/postgresql/root.crt";
+            # caCert = "/etc/postgresql/root.crt";
           };
           settings = {
             https-port = 14443;
