0cad88cb21273fd65ef792b67085dee1f47a313c
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-27 12:12:34 +0100

    NGINX on 444

--- haproxy.nix
+++ haproxy.nix
@@ -5,61 +5,70 @@ in
   services.haproxy = {
     enable = true;
     config = ''
-        daemon
-        maxconn 256
-        parse-resolv-conf
+  daemon
+  maxconn 256
 
-      defaults
-        mode http
-        timeout connect 5000ms
-        timeout client 50000ms
-        timeout server 50000ms
+defaults
+  mode tcp
+  timeout connect 5000ms
+  timeout client 50000ms
+  timeout server 50000ms
 
-      frontend http-in
-          bind *:82
-          bind [::]:82
-          default_backend http
+frontend http-in
+    bind *:82
+    bind [::]:82
+    default_backend http
 
-      frontend https-in
-        mode tcp
-        bind *:445
-        bind [::]:445
-        acl tls req.ssl_hello_type 1
-        tcp-request inspect-delay 5s
-        tcp-request content accept if tls
-        
+frontend https-in
+  mode tcp
+  bind *:445
+  bind [::]:445
+  acl tls req.ssl_hello_type 1
+  tcp-request inspect-delay 5s
+  tcp-request content accept if tls
+  use-backend %[req.ssl_sni,lower,map(/etc/haproxy_domain_back.map)]
+  default_backend https
 
-        default_backend https
+backend http 
+  server server1 127.0.0.1:8083 maxconn 32
+
+# backend https
+#   mode tcp
+#   use-server fs if req.ssl_sni -i fs.roses.gdvoisins.com
+#   use-server cp if req.ssl_sni -i cp.roses.gdvoisins.com
+#   use-server public.cp if req.ssl_sni -i public.cp.roses.gdvoisins.com
+#   use-server co if req.ssl_sni -i co.roses.gdvoisins.com
+#   use-server static if req.ssl_sni -i static.roses.gdvoisins.com
+#   use-server roses if req.ssl_sni -i roses.gdvoisins.com
+#   use-server fontenay if req.ssl_sni -i fontenay.gdvoisins.com
+
+backend https
+  mode tcp
+  server server1 fontenay.gdvoisins.com:443 maxconn 32
+
+backend fs
+  server fs fs.roses.gdvoisins.com:444
+
+backend cp
+  server cp cp.roses.gdvoisins.com:444
+
+backend public.cp
+  server public.cp public.cp.roses.gdvoisins.com:444
+
+backend co
+  server co co.roses.gdvoisins.com:444
+
+backend static
+  server static static.roses.gdvoisins.com:444
+
+backend roses
+  server roses roses.gdvoisins.com:444
+
+backend fontenay
+  server fontenay fontenay.gdvoisins.com:444
 
 
-      listen https-listen
-        mode tcp
-        bind :444
-        server dynamic %[req.ssl_sni,lower]:443
-        
 
-
-      backend http 
-        server server1 127.0.0.1:8083 maxconn 32
-
-
-      backend https
-        mode tcp
-        use-server fs if req.ssl_sni -i fs.roses.gdvoisins.com
-        use-server cp if req.ssl_sni -i cp.roses.gdvoisins.com
-        use-server public.cp if req.ssl_sni -i public.cp.roses.gdvoisins.com
-        use-server co if req.ssl_sni -i co.roses.gdvoisins.com
-        use-server static if req.ssl_sni -i static.roses.gdvoisins.com
-        use-server roses if req.ssl_sni -i roses.gdvoisins.com
-        use-server fontenay if req.ssl_sni -i fontenay.gdvoisins.com
-
-        server fs fs.roses.gdvoisins.com:443
-        server cp cp.roses.gdvoisins.com:443
-        server public.cp public.cp.roses.gdvoisins.com:443
-        server co co.roses.gdvoisins.com:443
-        server static static.roses.gdvoisins.com:443
-        server roses roses.gdvoisins.com:443
-        server fontenay fontenay.gdvoisins.com:443
 
     '';
   };
