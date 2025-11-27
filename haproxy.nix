65f0c7699e8bd663aca781bee66a0952a8a96231
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-27 18:30:47 +0100

    bind

--- haproxy.nix
+++ haproxy.nix
@@ -64,13 +64,13 @@ backend https
 backend https-back-4
   mode tcp
   # server server1 cp.roses.gdvoisins.com:445 maxconn 32
-  server server1 cp.roses.gdvoisins.com:445 maxconn 32 check resolver mynameservers
+  server server1 cp.roses.gdvoisins.com:445 maxconn 32 check resolvers mynameservers
   # server server1 127.0.0.1:445 maxconn 32
 
 backend https-back-6
   mode tcp
   # server server1 cp.roses.gdvoisins.com:445 maxconn 32 
-  server server1 cp.roses.gdvoisins.com:445 maxconn 32 check resolver mynameservers
+  server server1 cp.roses.gdvoisins.com:445 maxconn 32 check resolvers mynameservers
   # server server1 [::1]:445 maxconn 32
 
 backend fs
