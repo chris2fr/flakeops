7a992c6368cbf007e5d8c5084655b393de0e2f5a
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-27 09:34:14 +0100

    haproxy test

--- haproxy.nix
+++ haproxy.nix
@@ -19,11 +19,11 @@ in
           bind *:82
           default_backend server
 
-      frontend https-in
-          bind *:445 ssl
-          ssl-f-use crt /var/lib/acme/roses.lgv.info/fullchain.pem key /var/lib/acme/roses.lgv.info/key.pem
-          http-request return status 200 content-type text/plain lf-string "%[path,field(-1,/)].%[path,field(-1,/),map(virt@acme)]\n" if { path_beg '/.well-known/acme-challenge/' }
-          default_backend server
+      # frontend https-in
+      #     bind *:445 ssl
+      #     ssl-f-use crt /var/lib/acme/roses.lgv.info/fullchain.pem key /var/lib/acme/roses.lgv.info/key.pem
+      #     http-request return status 200 content-type text/plain lf-string "%[path,field(-1,/)].%[path,field(-1,/),map(virt@acme)]\n" if { path_beg '/.well-known/acme-challenge/' }
+      #     default_backend server
 
       backend server
           server server1 static.roses.gdvoisins.com:80 maxconn 32
