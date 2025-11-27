053c2b3b3a09fc73169e139fff8f63e7d1094b73
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-27 12:23:04 +0100

    no default listen

--- nginx.nix
+++ nginx.nix
@@ -81,12 +81,12 @@ in
       #     };
       #   };
       # };
-      defaultListen = [
-        { addr = "0.0.0.0" ; port = 444 ; ssl = true; extraParameters = ["proxy_protocol"]; }
-        # { addr = "[2a01:e0a:f4e:5880::9316:9fe2]" ; port = 444 ; ssl = true;  }
-        { addr = "0.0.0.0" ; port = 81; }
-        # { addr = "[2a01:e0a:f4e:5880::9316:9fe2]" ; port = 480; }
-      ];
+      # defaultListen = [
+      #   { addr = "0.0.0.0" ; port = 444 ; ssl = true; extraParameters = ["proxy_protocol"]; }
+      #   # { addr = "[2a01:e0a:f4e:5880::9316:9fe2]" ; port = 444 ; ssl = true;  }
+      #   { addr = "0.0.0.0" ; port = 81; }
+      #   # { addr = "[2a01:e0a:f4e:5880::9316:9fe2]" ; port = 480; }
+      # ];
 
       virtualHosts = {
         # "vouch.roses.gdvoisins.com" = {
