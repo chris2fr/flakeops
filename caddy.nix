a33b877ff7bdfd6d419d8ef30e32cd8d49740a3c
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-28 06:32:58 +0100

    caddy

--- caddy.nix
+++ caddy.nix
@@ -5,8 +5,8 @@ in
   services.caddy = {
     enable = true;
     package = pkgs.caddy.withPlugins {
-      plugins = ["github.com/authcrunch/authcrunch@v1.0.14"];
-      hash = "sha256-muPcuC9drM5kSkvUJny0d5nFH4Z6k9lDunpVpPDVU9M=";
+      plugins = ["greenpau/caddy-security@v1.1.31"];
+      hash = "";
     };
     user = "wwwrun";
     group = "wwwrun";
