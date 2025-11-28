1dbbddbba9d52dc871be9ae1bda16aa4628cc1e9
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-28 06:37:18 +0100

    caddy

--- caddy.nix
+++ caddy.nix
@@ -5,7 +5,7 @@ in
   services.caddy = {
     enable = true;
     package = pkgs.caddy.withPlugins {
-      plugins = ["github.com/greenpau/caddy-security@v1.1.31"];
+      plugins = ["github.com/greenpau/caddy-security@v1.1.31" "github.com/mholt/caddy-l4@v0.0.0-20251124224044-66170bec9f4d"];
       hash = "";
     };
     user = "wwwrun";
