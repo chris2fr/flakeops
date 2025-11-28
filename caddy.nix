8eb155f42e305468274fad303dc31d26ea5a5c21
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-28 06:39:01 +0100

    caddy

--- caddy.nix
+++ caddy.nix
@@ -5,8 +5,9 @@ in
   services.caddy = {
     enable = true;
     package = pkgs.caddy.withPlugins {
-      plugins = ["github.com/greenpau/caddy-security@v1.1.31" "github.com/mholt/caddy-l4@v0.0.0-20251124224044-66170bec9f4d"];
-      hash = "";
+      plugins = ["github.com/greenpau/caddy-security@v1.1.31"];
+      # plugins = ["github.com/greenpau/caddy-security@v1.1.31" "github.com/mholt/caddy-l4@v0.0.0-20251124224044-66170bec9f4d"];
+      hash = "sha256-b+hW1MN84eW7OkBIwKHp4VrvHOVi8gsTnTrWAoxmbE0=";
     };
     user = "wwwrun";
     group = "wwwrun";
