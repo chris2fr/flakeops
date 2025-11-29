61e0117bec3e7c5177c234a50787e2ebd2b44744
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-29 12:00:48 +0100

    acme

--- acme.nix
+++ acme.nix
@@ -9,28 +9,28 @@ in
       email = "chris@lesgrandsvoisins.com";
       webroot = null;
     };
-    certs = {
-      "roses.gdvoisins.com" = {
-        # webroot = "/var/lib/acme/acme-challenge/";
-        webroot = "/var/lib/acme/acme-challenge/";
-        # extraDomainNames = [ 
-        #   "www.roses.gdvoisins.com"
-        #   "cp.roses.gdvoisins.com" 
-        #   "co.roses.gdvoisins.com" 
-        #   "cw.roses.gdvoisins.com" 
-        #   "fs.roses.gdvoisins.com" 
-        #   "public.cp.roses.gdvoisins.com" 
-        #   "static.roses.gdvoisins.com" 
-        #   "fontenay.gdvoisins.com" 
-        # ];
-      };
-      "cp.roses.gdvoisins.com"  = {webroot = "/var/lib/acme/acme-challenge/";};
-      "co.roses.gdvoisins.com"   = {webroot = "/var/lib/acme/acme-challenge/";};
-      "cw.roses.gdvoisins.com"   = {webroot = "/var/lib/acme/acme-challenge/";};
-      "fs.roses.gdvoisins.com"   = {webroot = "/var/lib/acme/acme-challenge/";};
-      "public.cp.roses.gdvoisins.com"  = {webroot = "/var/lib/acme/acme-challenge/";}; 
-      "static.roses.gdvoisins.com" = {webroot = "/var/lib/acme/acme-challenge/";};
-      "fontenay.gdvoisins.com"   = {webroot = "/var/lib/acme/acme-challenge/";};
-    };
+    # certs = {
+    #   "roses.gdvoisins.com" = {
+    #     # webroot = "/var/lib/acme/acme-challenge/";
+    #     webroot = "/var/lib/acme/acme-challenge/";
+    #     # extraDomainNames = [ 
+    #     #   "www.roses.gdvoisins.com"
+    #     #   "cp.roses.gdvoisins.com" 
+    #     #   "co.roses.gdvoisins.com" 
+    #     #   "cw.roses.gdvoisins.com" 
+    #     #   "fs.roses.gdvoisins.com" 
+    #     #   "public.cp.roses.gdvoisins.com" 
+    #     #   "static.roses.gdvoisins.com" 
+    #     #   "fontenay.gdvoisins.com" 
+    #     # ];
+    #   };
+    #   "cp.roses.gdvoisins.com"  = {webroot = "/var/lib/acme/acme-challenge/";};
+    #   "co.roses.gdvoisins.com"   = {webroot = "/var/lib/acme/acme-challenge/";};
+    #   "cw.roses.gdvoisins.com"   = {webroot = "/var/lib/acme/acme-challenge/";};
+    #   "fs.roses.gdvoisins.com"   = {webroot = "/var/lib/acme/acme-challenge/";};
+    #   "public.cp.roses.gdvoisins.com"  = {webroot = "/var/lib/acme/acme-challenge/";}; 
+    #   "static.roses.gdvoisins.com" = {webroot = "/var/lib/acme/acme-challenge/";};
+    #   "fontenay.gdvoisins.com"   = {webroot = "/var/lib/acme/acme-challenge/";};
+    # };
   };
 }
