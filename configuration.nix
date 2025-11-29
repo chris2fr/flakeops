b9cc7bd90b1ecfd15cf1e3dca31a5b969870e455
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-29 12:07:46 +0100

    acme

--- configuration.nix
+++ configuration.nix
@@ -20,7 +20,7 @@ in
     # ./oauth2-proxy.nix
     # ./containers.nix
     # ./haproxy.nix
-    ./acme.nix
+    # ./acme.nix
     ./caddy.nix
   ];
   environment.systemPackages = with pkgs; [ 
@@ -247,11 +247,6 @@ in
       "d /srv/data9 0755 nfsuser users"
     ];
   };
-
-  # security.acme = {
-  #   acceptTerms = true;
-  #   defaults.email = "chris@lesgrandsvoisins.com";
-  # };
 
   services = {
     openssh = {
