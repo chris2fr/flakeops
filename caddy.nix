1a02ea7f2bfbd6cb8073023edaa4c74e7269413f
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-29 09:20:17 +0100

    Caddy

--- caddy.nix
+++ caddy.nix
@@ -9,7 +9,7 @@ in
       # plugins = ["github.com/greenpau/caddy-security@v1.1.31" "github.com/mholt/caddy-l4@v0.0.0-20251124224044-66170bec9f4d"];
       hash = "sha256-b+hW1MN84eW7OkBIwKHp4VrvHOVi8gsTnTrWAoxmbE0=";
     };
-    environmentFile = "/var/lib/copyparty/.env";
+    environmentFile = "/var/lib/caddy/.env";
     user = "wwwrun";
     group = "wwwrun";
     email = "hostmaster@lesgrandsvoisins.com";
