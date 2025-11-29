06e76be2a87831b0f9a6435318213c3a65778a52
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-29 13:06:43 +0100

    caddy

--- caddy.nix
+++ caddy.nix
@@ -3,7 +3,7 @@ let
 in
 { 
   services.caddy = {
-    enable = false;
+    enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = ["github.com/greenpau/caddy-security@v1.1.31"];
       # plugins = ["github.com/greenpau/caddy-security@v1.1.31" "github.com/mholt/caddy-l4@v0.0.0-20251124224044-66170bec9f4d"];
