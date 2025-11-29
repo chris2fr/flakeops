bbfbc776baf5dda47884a411c9212393f07fc3b1
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-29 08:31:37 +0100

    Caddy

--- configuration.nix
+++ configuration.nix
@@ -59,11 +59,11 @@ in
     # luajit_openresty
     # luajitPackages.lua-resty-openidc
     acme-sh
-    caddy.withPlugins {
-      plugins = ["github.com/greenpau/caddy-security@v1.1.31"];
-      # plugins = ["github.com/greenpau/caddy-security@v1.1.31" "github.com/mholt/caddy-l4@v0.0.0-20251124224044-66170bec9f4d"];
-      hash = "sha256-b+hW1MN84eW7OkBIwKHp4VrvHOVi8gsTnTrWAoxmbE0=";
-    }
+    # caddy.withPlugins {
+    #   plugins = ["github.com/greenpau/caddy-security@v1.1.31"];
+    #   # plugins = ["github.com/greenpau/caddy-security@v1.1.31" "github.com/mholt/caddy-l4@v0.0.0-20251124224044-66170bec9f4d"];
+    #   hash = "sha256-b+hW1MN84eW7OkBIwKHp4VrvHOVi8gsTnTrWAoxmbE0=";
+    # }
   ];
     users.users.wwwrun.extraGroups = [ "acme" ];
     users.users.nginx.extraGroups = [ "acme" ];
