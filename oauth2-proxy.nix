89563647c74d3945855414cced2518960973d2da
Chris Mann <chris@lesgtrandsvoisins.com>
2025-11-30 07:43:13 +0100

    oauth2-proxy

--- oauth2-proxy.nix
+++ oauth2-proxy.nix
@@ -5,7 +5,7 @@ let
     jwtVouchSecret = import ./secrets/jwt-vouch-secret.nix;
 in
 {
-    users.users.oauth2-proxy.extraGroups = [ "nginx" "acme" ];
+    users.users.oauth2-proxy.extraGroups = [ "nginx" "acme" "wwwrun";
 
     services.oauth2-proxy = {
       enable = true;
