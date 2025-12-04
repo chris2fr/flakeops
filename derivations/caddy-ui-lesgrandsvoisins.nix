e337c866bc4266e28ec4f0d3cd1b4f32f72319ba
Chris Mann <chris@lesgtrandsvoisins.com>
2025-12-04 11:57:05 +0100

    debug

--- derivations/caddy-ui-lesgrandsvoisins.nix
+++ derivations/caddy-ui-lesgrandsvoisins.nix
@@ -5,6 +5,12 @@ stdenv.mkDerivation rec {
   version = "1.0.2";
 
   src = ./caddy-ui-lesgrandsvoisins;
+
+  install_commands =   (lib.strings.concatMapStrings (x: "install -Dm644 ${src}/${x} out/${x}\n") [
+    "assets/portal/templates/lesgrandsvoisins/login.template"
+    "assets/images/logo-lesgrandsvoisins-800-400-white.png"
+    ] );
+
   installPhase = lib.strings.concatStrings [
     ''
     mkdir -p $out/assets/portal/templates/lesgrandsvoisins
@@ -14,5 +20,8 @@ stdenv.mkDerivation rec {
     "assets/portal/templates/lesgrandsvoisins/login.template"
     "assets/images/logo-lesgrandsvoisins-800-400-white.png"
     ] )
+    ''
+      echo "${install_commands}" > /tmp/debug
+    ''
   ];
 }
