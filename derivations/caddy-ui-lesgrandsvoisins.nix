f78d9ea29308f90d0e0a5a7ec08406384cf5439d
Chris Mann <chris@lesgtrandsvoisins.com>
2025-12-04 11:37:33 +0100

    simple trivial derviation

--- derivations/caddy-ui-lesgrandsvoisins.nix
+++ derivations/caddy-ui-lesgrandsvoisins.nix
@@ -5,14 +5,5 @@ stdenv.mkDerivation rec {
   version = "1.0.0";
 
   src = ./caddy-ui-lesgrandsvoisins/.;
-  installPhase = lib.strings.concatStrings [
-    ''
-    mkdir -p $out/assets/portal/templates/lesgrandsvoisins
-    mkdir -p $out/assets/images
-    ''
-    (lib.strings.concatMapStrings (x: "install -Dm644 ./${x} out/${x}\n") [
-    "assets/portal/templates/lesgrandsvoisins/login.template"
-    "assets/images/logo-lesgrandsvoisins-800-400-white.png"
-    ] )
-  ];
+
 }
