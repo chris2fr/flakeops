6856de5ff2a820658e4b57d95b567c8e29e9d225
Chris Mann <chris@lesgtrandsvoisins.com>
2025-12-04 11:52:20 +0100

    bumped the version number

--- derivations/caddy-ui-lesgrandsvoisins.nix
+++ derivations/caddy-ui-lesgrandsvoisins.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "caddy-ui-lesgrandsvoisins";
-  version = "1.0.1";
+  version = "1.0.2";
 
   src = ./caddy-ui-lesgrandsvoisins;
   installPhase = lib.strings.concatStrings [
@@ -10,7 +10,7 @@ stdenv.mkDerivation rec {
     mkdir -p $out/assets/portal/templates/lesgrandsvoisins
     mkdir -p $out/assets/images
     ''
-    (lib.strings.concatMapStrings (x: "install -Dm644 ./${x} out/${x}\n") [
+    (lib.strings.concatMapStrings (x: "install -Dm644 ${src}/${x} out/${x}\n") [
     "assets/portal/templates/lesgrandsvoisins/login.template"
     "assets/images/logo-lesgrandsvoisins-800-400-white.png"
     ] )
