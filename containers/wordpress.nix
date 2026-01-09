e6f3b6df9e8b985b94db1157ab2c2deff55ac704
Chris Mann <chris@lesgtrandsvoisins.com>
2026-01-09 04:28:09 +0100

    Home Manager SHA

--- containers/wordpress.nix
+++ containers/wordpress.nix
@@ -6,7 +6,7 @@
 }: let
   # home-manager = builtins.fetchTarball {
   #   url="https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
-  #   sha256="sha256:1fxgnwm6v22ygsfwynp3lszrgwqx10g1a6w3ypfk7ir0nfqfc9p5";
+  #   sha256="sha256:1rj0cazl5kjcfn4433fj31293yx421wbawryp5q3bq3fsmhkkr9h";
   # };
   # home-manager = import ../vars/home-manager.nix;
   mannchriRsaPublic = import ../vars/mannchri-rsa-public.nix;
--- vars/home-manager.nix
+++ vars/home-manager.nix
@@ -1,4 +1,4 @@
 builtins.fetchTarball {
   url = "https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
-  sha256 = "sha256:1fxgnwm6v22ygsfwynp3lszrgwqx10g1a6w3ypfk7ir0nfqfc9p5";
+  sha256 = "sha256:1rj0cazl5kjcfn4433fj31293yx421wbawryp5q3bq3fsmhkkr9h";
 }
