2665994a7d9adb9f8e32c75aaedc6411d806bdfe
Chris Mann <chris@mann.fr>
2026-01-29 16:00:28 +0100

    phylum

--- caddy.nix
+++ caddy.nix
@@ -219,10 +219,7 @@ in {
       };
       "phylum.roses.gdvoisins.com" = {
         extraConfig = ''
-          root * /var/www/phylum-client-build/web
           reverse_proxy /api/* 0.0.0.0:2448
-          reverse_proxy /login/* 0.0.0.0:2448
-          file_server
         '';
       };
     };
