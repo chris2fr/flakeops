efcc29a269f768339f537308aeddb603b2b9f2b6
Chris Mann <chris@mann.fr>
2026-01-29 14:54:29 +0100

    phylum

--- caddy.nix
+++ caddy.nix
@@ -221,7 +221,6 @@ in {
         extraConfig = ''
           root * /var/www/phylum-client-build/web
           reverse_proxy /api/* 0.0.0.0:2448
-          reverse_proxy /login/* 0.0.0.0:2448
           file_server
         '';
       };
