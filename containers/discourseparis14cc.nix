242684a0406f38baf5b8c14d317f96084cf4ef20
Chris Mann <chris@lesgtrandsvoisins.com>
2026-04-10 07:44:44 +0200

    Insecure python to correct

--- containers/discourseparis14cc.nix
+++ containers/discourseparis14cc.nix
@@ -54,6 +54,7 @@ in {
       ];
       nixpkgs.config.permittedInsecurePackages = [
         "discourse-3.2.5"
+        "python3.15-pypdf2-3.0.1"
       ];
       virtualisation.docker.enable = true;
       system.stateVersion = "25.11";
